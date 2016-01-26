61ef8fb0b4a957ab062df9ed220dc17777c0a831
josephwork <subins@gmail.com>
2016-01-26 12:43:04 +0000

    Code clean-up

--- app/controllers/ResponseController.scala
+++ app/controllers/ResponseController.scala
@@ -1,14 +1,10 @@
 package controllers
 
-import java.util.Date
 import javax.inject.Inject
 
-import model.{ResponseData, Response}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import service.ResponseService
-
-import scala.concurrent.Future
 
 /**
   * Created by Joseph Sebastian on 20/01/2016.
@@ -18,24 +14,8 @@ class ResponseController @Inject()(val responseService: ResponseService) extends
   import scala.concurrent.ExecutionContext.Implicits.global
 
   def all(namePrefix: String = "") = Action.async {
-    val result = responseService.allFuture(namePrefix)
-    result.map { responseDataList =>
+    responseService.all(namePrefix).map { responseDataList =>
       Ok(Json.toJson(responseDataList))
     }
   }
-
-  /*
-    def all(namePrefix:String="") = Action.async {
-      val (xmls, zips) = responseService.all(namePrefix)
-      val responseList = xmls.map(new ResponseData(_)) ++ zips.map(new ResponseData(_))
-
-  //    Ok(responseService.all(namePrefix))
-      scala.concurrent.Future {
-  //      Ok()
-  //      Ok(Json.toJson(ResponseData("te",true, "empty", new Date(), "xml")))
-        Ok(Json.toJson(responseList))
-      }
-    }
-
-  */
 }
--- app/service/ResponseService.scala
+++ app/service/ResponseService.scala
@@ -26,38 +26,14 @@ import scala.concurrent.duration._
   */
 @ImplementedBy(classOf[ResponseServiceImpl])
 trait ResponseService {
-  def all(filePrefix: String): (List[XmlFile], List[ZipFile])
-
-  def allFuture(filePrefix: String): Future[List[ResponseData]]
+  def all(filePrefix: String): Future[List[ResponseData]]
 }
 
 class ResponseServiceImpl @Inject()(val xmlService: XmlService, val zipService: ZipService) extends ResponseService {
 
   lazy val reactiveMongoApi = current.injector.instanceOf[ReactiveMongoApi]
 
-  def xmlCollection: JSONCollection = reactiveMongoApi.db.collection[JSONCollection]("xml")
-
-  def zipCollection: JSONCollection = reactiveMongoApi.db.collection[JSONCollection]("zip")
-
-  override def all(filePrefix: String): (List[XmlFile], List[ZipFile]) = {
-    val xmls = xmlService.findByCriteria(Map("name" -> Json.obj("$regex" -> JsString(filePrefix))), 100)
-    val zips = zipService.findByCriteria(Map("name" -> Json.obj("$regex" -> JsString(filePrefix))), 100)
-    val files = for {
-      xmlFiles <- xmls
-      zipFiles <- zips
-    } yield (xmlFiles, zipFiles)
-    Await.result(files, 10 seconds)
-
-    var response: (List[XmlFile], List[ZipFile]) = (List(), List())
-    files.onComplete {
-      //      case _ => (xmlF, zips)
-      case Success(responseFiles) => response = (responseFiles._1, responseFiles._2)
-      case Failure(ex) => println("empty list")
-    }
-    response
-  }
-
-  override def allFuture(filePrefix: String): Future[List[ResponseData]] = {
+  override def all(filePrefix: String): Future[List[ResponseData]] = {
     val xmls = xmlService.findByCriteria(Map("name" -> Json.obj("$regex" -> JsString(filePrefix))), 100)
     val zips = zipService.findByCriteria(Map("name" -> Json.obj("$regex" -> JsString(filePrefix))), 100)
     val files = for {
@@ -68,25 +44,4 @@ class ResponseServiceImpl @Inject()(val xmlService: XmlService, val zipService: 
       responseFiles._1.map(new ResponseData(_)) ++ responseFiles._2.map(new ResponseData(_))
     }
   }
-
-  /*
-
-    override def all(filePrefix: String): (List[XmlFile], List[ZipFile]) = {
-      val xmlFiles = xmlService.findByCriteria(Map("name" -> Json.obj("$regex" -> JsString(filePrefix))), 100)
-      //    { name: {$regex: 'test1'}}
-      //    val xmlFiles = xmlCollection.find(BSONDocument("name" -> BSONDocument("$regex" -> filePrefix))).cursor[XmlFile].collect[List]()
-      //    val zipFiles = xmlCollection.find(BSONDocument("name" -> BSONDocument("$regex" -> filePrefix))).cursor[ZipFile].collect[List]()
-
-      var xmlList: List[XmlFile] = List()
-      Await.result(xmlFiles, 10 seconds)
-      xmlFiles.onComplete {
-        //      case files:Success[List[XmlFile]]=> {
-        case Success(files) => xmlList = files.asInstanceOf[List[XmlFile]]
-        case _ => List()
-      }
-      Await.result(xmlFiles, 10 seconds)
-
-      (xmlList, List())
-    }
-  */
 }
